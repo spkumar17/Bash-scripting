ef6165fd36fd17701c27950dd897bfaec8436a27
prasannakumar <prasannakumarsinganamalla@gmail.com>
2024-10-07 17:53:09 +0530

    modified dockerstatuscheck.sh  file

--- shellscripting/dockerstatuscheck.sh
+++ shellscripting/dockerstatuscheck.sh
@@ -1,8 +1,37 @@
 #!/bin/bash
  
-
+# this will check if the docker is active if not it will start the service 
+# running this script using while not with cron   
 while true; do
     check=$(systemctl status docker | awk 'NR==3 {print}' | cut -d '(' -f 1 | cut -d ':' -f 2 | xargs)
+
+    if [ "$check" == "active" ]; then
+        echo "Docker service is active"
+    else
+        echo "Docker service is inactive"
+        sudo usermod -aG docker $USER
+        check2=$(systemctl status docker | awk 'NR==3 {print}')
+        echo $check2
+        echo "starting docker services..."
+        sudo systemctl start docker
+        sleep 3
+        check=$(systemctl status docker | awk 'NR==3 {print}' | cut -d '(' -f 1 | cut -d ':' -f 2 | xargs)
+        if [ "$check" == "active" ]; then
+            echo "Docker service is active"
+        else
+            echo " unable to start the docker service"
+            echo " take required action......."
+            exit 1
+        fi    
+
+
+    fi
+    sleep 10         
+done
+---
+
+
+check=$(systemctl status docker | awk 'NR==3 {print}' | cut -d '(' -f 1 | cut -d ':' -f 2 | xargs)
 
     if [ "$check" == "active" ]; then
         echo "Docker service is active"
@@ -24,5 +53,3 @@ while true; do
 
 
     fi
-    sleep 10         
-done
