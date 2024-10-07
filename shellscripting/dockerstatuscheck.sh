6e7ea0be371cf3d17933e2fc42da25b30b73a472
prasannakumar <prasannakumarsinganamalla@gmail.com>
2024-10-07 17:39:22 +0530

    modified dockerstatuscheck.sh  file

--- shellscripting/dockerstatuscheck.sh
+++ shellscripting/dockerstatuscheck.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+ 
 
 while true; do
     check=$(systemctl status docker | awk 'NR==3 {print}' | cut -d '(' -f 1 | cut -d ':' -f 2 | xargs)
@@ -8,7 +8,21 @@ while true; do
         echo "Docker service is active"
     else
         echo "Docker service is inactive"
-        systemctl status docker
+        check2=$(systemctl status docker | awk 'NR==3 {print}')
+        echo $check2
+        echo "starting docker services..."
+        sudo systemctl start docker
+        sleep 3
+        check=$(systemctl status docker | awk 'NR==3 {print}' | cut -d '(' -f 1 | cut -d ':' -f 2 | xargs)
+        if [ "$check" == "active" ]; then
+            echo "Docker service is active"
+        else
+            echo " unable to start the docker service"
+            echo " take required action......."
+            exit 1
+        fi    
+
+
     fi
     sleep 10         
 done
