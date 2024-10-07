d5bd73a180d8482c336862b337910ba809a2e186
prasannakumar <prasannakumarsinganamalla@gmail.com>
2024-10-07 22:06:44 +0530

    commited fetchlogs  file

--- shellscripting/fetch-errors-inlogs.sh
+++ shellscripting/fetch-errors-inlogs.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 read -p "enter the file path:" Fetch
-if [ -z "$Fetch" ]; then 
+if [ -z "$Fetch" ]; then
     echo "enter the path"
 elif [ ! -f "$Fetch" ]; then
-    echo "File not found or incorrect path. Please check the path and try again."    
-else    
+    echo "File not found or incorrect path. Please check the path and try again."
+else
     Search=$(grep -w ERROR $Fetch )
     if [ -z "$Search" ]; then
         echo "No error logs in the given log file"
     else
         grep -w ERROR $Fetch >> /home/kumar/shellscripting/AppErrorLogs$(date +"%Y-%m-%d_%H-%M-%S").log
-        echo "Moved error logs to /home/kumar/shellscripting/AppErrorLogs$(date +"%Y-%m-%d_%H-%M-%S").log"
-    fi                                                                                                                                    
-fi    
+        echo "Moved error logs to AppErrorLogs$(date +"%Y-%m-%d_%H-%M-%S").log file in /home/kumar/shellscripting directory")           fi
+
+fi
