2b3dfd28dda719d8b4bc3a36abe9d1d51743cdfe
prasannakumar <prasannakumarsinganamalla@gmail.com>
2024-10-12 12:37:48 +0530

    modifed this   file

--- shellscripting/mving-logfiles.sh
+++ shellscripting/mving-logfiles.sh
@@ -1,41 +1,33 @@
-#!/bin/bash
 
-# This script moves log files which are created before 30 days 
-
-# Example if all the logs files are stored in /home/kumar/shellscripting
-
-path=$1
-
-if [ -z "$path" ]; then 
+if [ -z "$path" ]; then
     echo "Please provide the path."
     exit 1
 else
-    Backup_path="/home/kumar/shellscriptingbackup"
+    TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
+    Backup_path="/home/kumar/shellscripting/shellscripting_backup"
 
-    mkdir -p "$Backup_path"  # this will create a backup directory if not present 
+    mkdir -p "$Backup_path"  # this will create a backup directory if not present
 
     # Find files older than 30 days and store them in an array
     Tempfiles=($(find "$path" -type f -mtime +30))
 
-    if [ ${#Tempfiles[@]} -eq 0 ]; then 
+    if [ ${#Tempfiles[@]} -eq 0 ]; then
         echo "No log files created in the past 30 days."
-    else 
+    else
         echo "Moving all the log files created 30 days ago to Backup_path: $Backup_path"
-        for Tempfile in "${Tempfiles[@]}"; do 
-            #cp "$Tempfile" "$Backup_path"  
-            mv "$Tempfile" "$Backup_path"
+        for Tempfile in "${Tempfiles[@]}"; do
+            #mv "$Tempfile" "$Backup_path"
+            cp "$Tempfile" "$Backup_path" # as a demo i am using cp but we need to use mv
             echo "$Tempfile moved to Backup_path: $Backup_path"
         done
     fi
 fi
 
+
 backup_Tempfiles=($(find "$Backup_path" -type f -mtime +30))
 
-if [ -z "$backup_Tempfiles" ]; then 
+if [ -z "$backup_Tempfiles" ]; then
     echo "No backup files to move to S3 Glacier"
 else
-    aws s3 mv $Backup_path s3://log-backup-cppe/backuplogs/ --storage-class GLACIER --recursive
-fi     
-
-
-
+    aws s3 cp $Backup_path s3://log-backup-cppe/backuplogs/ --storage-class GLACIER --recursive
+fi
